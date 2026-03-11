571b8409b28dc846bb84b4859415a477aa0d180e
Pedro Henrique Peçanha <pedrinho.top07@gmail.com>
2026-03-10 23:37:38 -0300

    Cria app Angular com telas de login, cadastro e upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UploadComponent } from './components/upload/upload.component';
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'cadastro', component: RegisterComponent },
+  { path: 'upload', component: UploadComponent },
+  { path: '**', redirectTo: '' }
+];
